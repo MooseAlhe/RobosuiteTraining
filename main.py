c6a1c95f55f1aa4f9abbd9a1be57aabf09de15ec
Mustafa Alhelawe <mustafa.alhelawe@gmail.com>
2025-04-21 00:14:57 -0400

    Update environment setup in main.py and change PyTorch extra index URL in requirements.txt

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import gym
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-#import robosuite as suite
-#from robosuite.wrappers import GymWrapper
+import robosuite as suite
+from robosuite.wrappers import GymWrapper
 from networks import CriticNetwork, ActorNetwork
 from buffer import ReplayBuffer
 
@@ -13,22 +13,22 @@ if __name__ == "__main__":
     if not os.path.exists("tmp/td3"):
         os.makedirs("tmp/td3")
 
-    # env_name = "Door"
-    #
-    # env = suite.make(
-    #     env_name,
-    #     robot=["Panda"],
-    #     controller_config=suite.load_controller_config(
-    #         default_controller="JOINT_VELOCITY"
-    #     ),
-    #     has_renderer=False,
-    #     use_camera_obs=False,
-    #     horizon=300,
-    #     reward_shaping=True,
-    #     control_freq=20,
-    # )
-    #
-    # env = GymWrapper(env)
+    env_name = "Door"
+
+    env = suite.make(
+        env_name,
+        robots=["Panda"],
+        controller_configs=suite.load_controller_config(
+            default_controller="JOINT_VELOCITY"
+        ),
+        has_renderer=False,
+        use_camera_obs=False,
+        horizon=300,
+        reward_shaping=True,
+        control_freq=20,
+    )
+
+    env = GymWrapper(env)
 
     ###
     critic_network = CriticNetwork([8], 8)
